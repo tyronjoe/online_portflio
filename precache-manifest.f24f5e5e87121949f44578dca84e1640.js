e9ce0f29214788518fe96fefe8e0d3ac74d21d0a
Tyron Joe <tyronjoe55@gmail.com>
2020-07-01 18:17:21 +0200

    Updates

---
+++ precache-manifest.f24f5e5e87121949f44578dca84e1640.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3465b9c92e8ed0af709c8d8a1a5fc241",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "0ac7a38594f8a400847e",
+    "url": "/my-app/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "f8ce0ca91060988405f2",
+    "url": "/my-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0ac7a38594f8a400847e",
+    "url": "/my-app/static/js/2.914a4bcb.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/my-app/static/js/2.914a4bcb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8ce0ca91060988405f2",
+    "url": "/my-app/static/js/main.5b89fa41.chunk.js"
+  },
+  {
+    "revision": "3ac687425fa640352900",
+    "url": "/my-app/static/js/runtime-main.1c83866c.js"
+  },
+  {
+    "revision": "8eb5203cfbbbe028976a393530a95cfb",
+    "url": "/my-app/static/media/naruto_4_tails.8eb5203c.gif"
+  }
+]);
